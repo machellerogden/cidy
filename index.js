c8ed0976baf0d13848637d6c35b71f785fa50f6d
Mac Heller-Ogden <mac@machellerogden.com>
2018-07-22 22:33:44 -0500

    WIP

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 'use strict';
-
 const Targets = require('targets');
 const requireDir = require('require-dir');
+const mapValues = require('lodash/mapValues');
+
+const createCommand = require('./lib/createCommand');
+
+const commands = mapValues(requireDir('./commands'), createCommand);
 const targets = requireDir('./targets');
 
-Targets({ name: 'cidy', targets });
+Targets({ name: 'cidy', targets: { ...commands, ...targets } });
 
--- targets/docker.build.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const createCommand = require('../lib/createCommand');
-const dockerCommand = require('../commands/docker.build');
-
-module.exports = createCommand(dockerCommand);
